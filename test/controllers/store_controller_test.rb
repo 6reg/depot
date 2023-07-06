e3f6244a90d561405574e861264b509c400f9429
Greg Mathias <mathiasgreg@gmail.com>
2023-07-06 16:01:07 -0400

    C4

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,12 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'nav.side_nav a', minimum: 4
+    assert_select 'main ul.catalog li', 3
+    assert_select '.price', /\$[,\d]+\.\d\d/ # dollar sign followed by at least one numbers,
+    # commas, or digits, followed by decimal point and 2 digits $12.99 /\$[,\d\]+\.\d\d/]
+    assert_select 'div.price', minimum: 1
+    assert_select 'h2', 'MyString'
   end
+
 end
