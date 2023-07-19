d21d0aaf0ce46a5a31784168f083ed191ced5c24
Greg Mathias <mathiasgreg@gmail.com>
2023-07-19 11:54:13 -0400

    Idk

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -47,7 +47,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
         description: @product.description,
         image_url: @product.image_url,
         price: @product.price,
-        title: @product.title
+        title: @title
       }
     }
     assert_redirected_to product_url(@product)
@@ -64,4 +64,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should go back to products" do
     get products_url
   end
+
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+
 end
